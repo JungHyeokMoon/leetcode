94c7857e4381e343d27bd27fef6dac43b741d2e0
문정혁 <39078904+JungHyeokMoon@users.noreply.github.com>
2022-03-15 21:01:40 +0900

    Time: 0 ms (100.00%), Space: 44.5 MB (17.07%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int product=1;
+        for(int num : nums){
+            if(num==0){
+                return 0;
+            }
+            
+            if(num>0){
+                product*=1;
+            }
+            else{
+                product*=-1;
+            }
+        }
+        
+        return product;
+    }
+}
