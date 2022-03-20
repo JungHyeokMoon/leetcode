203dad1f450bff6f4f3405adc401d2affc42fb16
문정혁 <39078904+JungHyeokMoon@users.noreply.github.com>
2022-03-20 19:51:41 +0900

    Time: 11 ms (13.48%), Space: 42.9 MB (5.78%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        Map<Character, Integer> map=new HashMap<>();
+        for(char c : t.toCharArray()){
+            map.compute(c, (k,v) -> v==null ? 1 : v+1);
+        }
+        for(char c : s.toCharArray()){
+            map.compute(c, (k,v) -> v==1 ? null :v-1);
+        }
+        return map.keySet().stream().findFirst().get().charValue();
+    }
+}
