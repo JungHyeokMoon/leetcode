3b980aef29f4dbb72f4aac206b8545e22e2efcf9
문정혁 <39078904+JungHyeokMoon@users.noreply.github.com>
2022-04-03 16:47:15 +0900

    Time: 17 ms (36.24%), Space: 56 MB (45.34%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,12 +1,11 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<Map<Character,Integer>, List<String>> map=new HashMap<>();
+        Map<String, List<String>> map=new HashMap<>();
         for(String str : strs){
-            Map<Character, Integer> countMap=new HashMap();
-            for(char c : str.toCharArray()){
-                countMap.compute(c,(k,v)-> v==null ? 1: v+1);
-            }
-            map.compute(countMap, (k, v) -> v == null ? new ArrayList<>() : v).add(str);
+            char[] chars=str.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr=new String(chars);
+            map.compute(sortedStr, (k, v) -> v == null ? new ArrayList<>() : v).add(str);
         }
         return new ArrayList<>(map.values());
 
