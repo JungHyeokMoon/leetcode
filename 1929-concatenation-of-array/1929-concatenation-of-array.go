aa0c6f22ea162bb924225feaed88074aebd11f5d
문정혁 <39078904+JungHyeokMoon@users.noreply.github.com>
2022-07-09 14:50:16 +0900

    Time: 18 ms (26.04%), Space: 6.3 MB (79.85%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.go
+++ 1929-concatenation-of-array/1929-concatenation-of-array.go
@@ -1,10 +1,3 @@
 func getConcatenation(nums []int) []int {
-	numsLength := len(nums)
-	ret := make([]int, numsLength*2)
-
-	for i := 0; i < numsLength; i++ {
-		ret[i] = nums[i]
-		ret[i+numsLength] = nums[i]
-	}
-	return ret
+    return append(nums, nums...)
 }
