d09daf77db40c3a5f64f73edb843663503609817
문정혁 <39078904+JungHyeokMoon@users.noreply.github.com>
2022-07-09 17:53:06 +0900

    Time: 12 ms (7.69%), Space: 3.5 MB (19.05%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.go
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.go
@@ -2,13 +2,11 @@ func finalValueAfterOperations(operations []string) int {
     ans := 0
     
     for _, op := range operations {
-        if(op[0]=='+' || op[2]=='+'){
+        if(op[1]=='+'){
             ans++
+        }else{
+            ans--
         }
-
-		if(op[0]=='-' || op[2]=='-'){
-			ans--
-		}
     }
 
 	return ans
