dd14d16f6e2db829cb481d3662706ea0f2e0c378
문정혁 <39078904+JungHyeokMoon@users.noreply.github.com>
2022-07-24 19:52:36 +0900

    Time: 130 ms (12.20%), Space: 8.3 MB (37.67%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.go
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.go
@@ -1,17 +1,18 @@
 func findDisappearedNumbers(nums []int) (ret []int) {
 
-	s := make(map[int]bool)
-
-	for i := 1; i <= len(nums); i++ {
-		s[i] = true
-	}
-
 	for _, val := range nums {
-		delete(s, val)
+
+		val = int(math.Abs(float64(val)))
+
+		if nums[val-1] > 0 {
+			nums[val-1] = -nums[val-1]
+		}
 	}
 
-	for val := range s {
-		ret = append(ret, val)
+	for idx, val := range nums {
+		if val > 0 {
+			ret = append(ret, idx+1)
+		}
 	}
 
 	return
